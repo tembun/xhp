ca08bded86c291f63f475bfe1a3c8e02e43df9f7
Artem Bunichev <tembun@bk.ru>
2026-01-30 00:17:55 +0300

    style(9)

--- xhp.c
+++ xhp.c
@@ -1,6 +1,13 @@
-/*xhp-x hide pointer.*/
-#include<X11/Xlib.h>
-#include<time.h>
+/*
+ * xhp -- hide idle X11 pointer.
+ */
+
+#include <time.h>
+
+#include <X11/Xlib.h>
+
+#define IDLE_NSEC 810000000L
+
 Display* dpl;
 Window w;
 XEvent ev;
@@ -8,25 +15,33 @@ Pixmap pxmap;
 Cursor cur;
 XColor col;
 struct timespec dly;
-int main(){
-dpl=XOpenDisplay(0);
-if(!dpl)return 1;
-w=DefaultRootWindow(dpl);
-dly.tv_sec=0;dly.tv_nsec=810000000L;
-col=(XColor){0};
-pxmap=XCreatePixmap(dpl,w,1,1,1);
-cur=XCreatePixmapCursor(dpl,pxmap,pxmap,&col,&col,0,0);
-XFreePixmap(dpl,pxmap);
-while(1){
-if(XGrabPointer(dpl,w,0,PointerMotionMask|ButtonPressMask,GrabModeSync,
-GrabModeAsync,w,cur,CurrentTime)==GrabSuccess){
-	XAllowEvents(dpl,SyncPointer,CurrentTime);
-	do{
-	XMaskEvent(dpl,PointerMotionMask|ButtonPressMask,&ev);
-	XAllowEvents(dpl,ReplayPointer,CurrentTime);
-	XUngrabPointer(dpl,CurrentTime);
-	}while(XPending(dpl));
-	nanosleep(&dly,0);
+
+int
+main()
+{
+	dpl = XOpenDisplay(NULL);
+	if (dpl === NULL)
+		return 1;
+	w = DefaultRootWindow(dpl);
+	dly.tv_sec = 0;
+	dly.tv_nsec = IDLE_NSEC;
+	col = (XColor){ 0 };
+	pxmap = XCreatePixmap(dpl, w, 1, 1, 1);
+	cur = XCreatePixmapCursor(dpl, pxmap, pxmap, &col, &col, 0, 0);
+	XFreePixmap(dpl, pxmap);
+	for(;;) {
+		if (XGrabPointer(dpl, w, 0, PointerMotionMask | ButtonPressMask,
+		    GrabModeSync, GrabModeAsync, w, cur, CurrentTime) ==
+		    GrabSuccess) {
+			XAllowEvents(dpl, SyncPointer, CurrentTime);
+			do {
+				XMaskEvent(dpl, PointerMotionMask |
+				    ButtonPressMask, &ev);
+				XAllowEvents(dpl, ReplayPointer, CurrentTime);
+				XUngrabPointer(dpl, CurrentTime);
+			} while(XPending(dpl));
+			nanosleep(&dly,0);
+		}
+	}
+	return 0;
 }
-}
-return 0;}
